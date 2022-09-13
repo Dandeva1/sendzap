aa86e4be5a7dc5b8abf75dc315d75ad148d3d203
Dandeva1 <108388361+Dandeva1@users.noreply.github.com>
2022-09-13 20:16:11 -0300

    Update index.js

--- frontend/src/components/TicketsList/index.js
+++ frontend/src/components/TicketsList/index.js
@@ -198,7 +198,7 @@ const TicketsList = (props) => {
 
     const shouldUpdateTicket = (ticket) =>
       (!ticket.userId || ticket.userId === user?.id || showAll) &&
-      (!ticket.queueId || selectedQueueIds.indexOf(ticket.queueId) > -1);
+      ((!ticket.queueId && showAll) || selectedQueueIds.indexOf(ticket.queueId) > -1);
 
     const notBelongsToUserQueues = (ticket) =>
       ticket.queueId && selectedQueueIds.indexOf(ticket.queueId) === -1;
